22db8ba8edd11ff5593544dc63990f5e63dfe77c
Kritik <kandwalktitik400@gmail.com>
2025-06-20 21:53:01 +0530

    Add project files.

---
+++ MySQLTestProject/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySQLTestProject.Data;
+using System.Linq;
+
+namespace MySQLTestProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateQuery(string userInput)
+        {
+            string sqlQuery = "";
+            List<Employee> result = new List<Employee>();
+
+            // Very basic rule-based parsing
+            if (userInput.ToLower().Contains("between"))
+            {
+                var words = userInput.Split(' ');
+                if (int.TryParse(words[words.Length - 3], out int start) && int.TryParse(words[words.Length - 1], out int end))
+                {
+                    sqlQuery = $"SELECT * FROM Employee WHERE EmpID BETWEEN {start} AND {end}";
+                    result = _context.Employees
+                        .Where(e => e.EmpID >= start && e.EmpID <= end)
+                        .ToList();
+                }
+                else
+                {
+                    sqlQuery = "Could not parse input properly.";
+                }
+            }
+            else
+            {
+                sqlQuery = "Sorry, I could not understand your query.";
+            }
+
+            ViewBag.GeneratedQuery = sqlQuery;
+            ViewBag.Result = result;
+
+            return View("Index");
+        }
+    }
+}
---
+++ MySQLTestProject/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MySQLTestProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        // 👇 Add this method *inside* AppDbContext class
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("employee"); // <-- exact match with MySQL
+        }
+    }
+
+    public class Employee
+    {
+        [Key]
+        public int EmpID { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
